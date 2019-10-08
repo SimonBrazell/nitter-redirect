0613611b2cb09065394314c7a50a3001e85d1137
SimonBrazell <simon@brazell.com.au>
2019-10-08 11:23:01 +1100

    Added popup menu with options to toggle redirects and set custom instance

--- background.js
+++ background.js
@@ -1,11 +1,35 @@
-const nitter = "https://nitter.net";
+'use strict';
+
+const nitterDefault = 'https://nitter.net';
+
+let instance;
+let nitterDisabled;
+
+chrome.storage.sync.get(
+  ['nitterDisabled', 'instance'],
+  (result) => {
+    nitterDisabled = result.nitterDisabled;
+    instance = result.instance || nitterDefault;
+  }
+);
+
+chrome.storage.onChanged.addListener(function (changes) {
+  if ('instance' in changes) {
+    instance = changes.instance.newValue || nitterDefault;
+  }
+  if ('nitterDisabled' in changes) {
+    nitterDisabled = changes.nitterDisabled.newValue;
+  }
+});
 
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
-    return {
-      redirectUrl:
-        nitter + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]
-    };
+    if (!nitterDisabled) {
+      return {
+        redirectUrl:
+          instance + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]
+      };
+    }
   },
   {
     urls: [
